a02388a36088318db197e34b4fde277de028ca9b
Kishore DKG <kishore@dkgrouplabs.com>
2026-02-21 17:50:46 +0530

    Base url change

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -12,7 +12,7 @@
 
 // Get API URL from environment variable or fallback to Azure production URL
 // Dynamic API URL switching
-// const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 export const API_BASE_URL = isLocal
     ? 'http://localhost:8080/sarthi-backend'
     : 'https://sarthibackendservice-bfe2eag3byfkbsa6.canadacentral-01.azurewebsites.net/sarthi-backend';

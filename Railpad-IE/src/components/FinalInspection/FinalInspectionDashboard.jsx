431065fb37fd352183a08720a7f457b40754d993
JayaKishore5 <kishore@dkgrouplabs.com>
2026-08-11 13:45:04 +0530

    dropdown value railpad

--- Railpad-IE/src/components/FinalInspection/FinalInspectionDashboard.jsx
+++ Railpad-IE/src/components/FinalInspection/FinalInspectionDashboard.jsx
@@ -426,6 +426,7 @@ const FinalInspectionDashboard = ({ user, isShiftActive, call, onUpdateCall, onP
     'RDSO/T-8327': { type: '6.2mm CGRSP', max: 154 },
     'RDSO/T-8528': { type: '10mm CGRSP', max: 445 },
     'RDSO/T-8747': { type: '10mm CGRSP', max: 425 },
+    'RDSO/T-8998': { type: '10mm CGRSP', max: 445 },
   };
 
   const getHardnessTolerance = (lotId) => {
